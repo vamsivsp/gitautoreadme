0694a0f2d747e421a8cd3b1f750adad3f6778fcd
vamsivsp <39555418+vamsivsp@users.noreply.github.com>
2024-07-02 12:54:23 +0530

    Update generate-readme.sh

--- generate-readme.sh
+++ generate-readme.sh
@@ -1,11 +1,25 @@
 bash
 #!/bin/bash
 
-# Extract resource information from Terraform code
-resource_info=$(terraform-docs --format=json)
+# # Extract resource information from Terraform code
+# resource_info=$(terraform-docs --format=json)
 
-# Generate README content using handlebars
-readme_content=$(handlebars -t README.tpl {{resource_info}})
+# # Generate README content using handlebars
+# readme_content=$(handlebars -t README.tpl {{resource_info}})
 
-# Output the generated README content to a file
-echo "$readme_content" > README.md
+# # Output the generated README content to a file
+# echo "$readme_content" > README.md
+# Parse Terraform configuration file
+
+terraform_resources=$(terraform show -json)
+
+# Extract resource information
+resources=($(echo "$terraform_resources" | jq -r '.resource[] | .name'))
+
+# Generate README content
+for resource in "${resources[@]}"; do
+  echo "## $resource"
+  echo ""
+  terraform show -json "$resource" | jq -r '.attributes[] | .name + ": " + .value'
+  echo ""
+done
