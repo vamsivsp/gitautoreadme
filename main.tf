72a89e823f321d7eb084d7fe9479dae37f97012c
tvs <you@example.com>
2024-07-02 12:51:39 +0530

    added main

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example1" {
   ami           = "ami-01376101673c89611"
   instance_type = "t2.micro"
 }
